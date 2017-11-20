2c22df2079745a363abeb38836b7ee5c4c1c4dd9
kellybarber <kelly.james.barber@gmail.com>
2017-11-20 15:20:33 -0500

    reorganizing files to better serve static assets

--- Web-RTC Project/server.js
+++ Web-RTC Project/server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const path    = require('path')
 const app     = express()
 
+app.use(express.static('js'))
+app.use(express.static('css'))
 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Listening on Port 3000')
+})
