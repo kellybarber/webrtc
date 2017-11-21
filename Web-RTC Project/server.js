d6ef6e52d7360aa94813b458553cbb2d5b154948
kellybarber <kelly.james.barber@gmail.com>
2017-11-20 19:06:55 -0500

    static files are being served correctly, video is working

--- Web-RTC Project/server.js
+++ Web-RTC Project/server.js
@@ -2,13 +2,7 @@ const express = require('express')
 const path    = require('path')
 const app     = express()
 
-app.use(express.static('js'))
-app.use(express.static('css'))
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
-})
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log('Listening on Port 3000')
