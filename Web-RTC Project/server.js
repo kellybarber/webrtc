62349e7f331038bab09b0d1d91bffd12f156dbbf
kellybarber <kelly.james.barber@gmail.com>
2017-11-21 20:47:41 -0500

    edited out redundant code on the server

--- Web-RTC Project/server.js
+++ Web-RTC Project/server.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const path    = require('path')
 const app     = express()
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log('Listening on Port 3000')
-})
+app.listen(3000, () => {console.log('Listening on Port 3000')})
